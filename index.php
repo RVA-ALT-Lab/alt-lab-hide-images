f8bb6d2aea5f9a46aa48c0e651b63697148b85c5
Tom Woodward <twwoodward@me.com>
2020-07-21 15:20:05 -0400

    switched to php for acf

--- index.php
+++ index.php
@@ -36,13 +36,18 @@ function hide_images_timer_builder($content){
           // Load sub field value.
           $image = get_sub_field('image');
           $time = get_sub_field('time_to_show');
+          if(get_sub_field('alt_text')){
+            $alt = get_sub_field('alt_text');
+          } else {
+            $alt = "An image for sketching.";
+          }
           // Do something...
 
         $html .= '<div class="timed-image-holder" data-show="' . $time . '">';
         $html .= '<div class="show-time">You will have ' . $time . ' seconds</div>';
         $html .= '<button class="timer">Start</button>';
         $html .= '<div class="progress-bar"></div>';
-        $html .= '<img src="' . $image . '" class="hide">';
+        $html .= '<img src="' . $image . '" class="hide" alt="' . $alt . '">';
         $html .= '<div class="finished"><img class="sunset" src="' . plugin_dir_url(__FILE__) . '/imgs/sunset.png" alt="A setting sun icon."><h2>Time has expired.<h2></div>';
         $html .= '</div>';
 
@@ -74,6 +79,118 @@ if ( ! function_exists('write_log')) {
 
   //print("<pre>".print_r($a,true)."</pre>");
 
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+  'key' => 'group_5f160cbe25d5b',
+  'title' => 'Images',
+  'fields' => array(
+    array(
+      'key' => 'field_5f160cc9397b9',
+      'label' => 'Image Data',
+      'name' => 'image_data',
+      'type' => 'repeater',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array(
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'collapsed' => '',
+      'min' => 0,
+      'max' => 0,
+      'layout' => 'table',
+      'button_label' => '',
+      'sub_fields' => array(
+        array(
+          'key' => 'field_5f160cd3397ba',
+          'label' => 'Image',
+          'name' => 'image',
+          'type' => 'image',
+          'instructions' => '',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array(
+            'width' => '30',
+            'class' => '',
+            'id' => '',
+          ),
+          'return_format' => 'url',
+          'preview_size' => 'medium',
+          'library' => 'all',
+          'min_width' => '',
+          'min_height' => '',
+          'min_size' => '',
+          'max_width' => '',
+          'max_height' => '',
+          'max_size' => '',
+          'mime_types' => '',
+        ),
+        array(
+          'key' => 'field_5f160d08397bb',
+          'label' => 'Time to Show',
+          'name' => 'time_to_show',
+          'type' => 'number',
+          'instructions' => 'In seconds',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array(
+            'width' => '20',
+            'class' => '',
+            'id' => '',
+          ),
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'min' => '',
+          'max' => '',
+          'step' => '',
+        ),
+        array(
+          'key' => 'field_5f160d41397bc',
+          'label' => 'Alt Text',
+          'name' => 'alt_text',
+          'type' => 'text',
+          'instructions' => 'Describe your image in a way that\'s meaningful for those who may have trouble viewing it.',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+          ),
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'maxlength' => '',
+        ),
+      ),
+    ),
+  ),
+  'location' => array(
+    array(
+      array(
+        'param' => 'post_type',
+        'operator' => '==',
+        'value' => 'post',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+  'active' => true,
+  'description' => '',
+));
+
+endif;
 
 
  //ACF JSON SAVER
